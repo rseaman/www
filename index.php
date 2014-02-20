88ebcdb40a889b59159784b8c66b7e7244cdea9d
Riley Seaman <riley.seaman@gmail.com>
2014-02-19 18:20:56 -0600

    Up to step 6

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 		<title>Nothing to see here...</title>
         <link rel="stylesheet" type="text/css" href="sheet.css"/>
@@ -8,5 +9,33 @@
 	</head>
 	<body>
 		<div id="titlebar"><strong><center>HEY!</center></strong></div>
+        <div class="container">
+             
+        <a class="toggleMenu" href="#">Menu</a>
+        <ul class="nav">
+            <li  class="test">
+                <a href="#">Shoes</a>
+                <ul>
+                    <li>
+                        <a href="#">Womens</a>
+                        <ul>
+                            <li><a href="#">Sandals</a></li>
+                            <li><a href="#">Sneakers</a></li>
+                            <li><a href="#">Wedges</a></li>
+                            <li><a href="#">Heels</a></li>
+                            <li><a href="#">Loafers</a></li>
+                            <li><a href="#">Flats</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Mens</a>
+                        <ul>
+                            <li><a href="#">Loafers</a></li>
+                            <li><a href="#">Sneakers</a></li>
+                            <li><a href="#">Formal</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
 	</body>
 </html>
--- script.js
+++ script.js
@@ -5,4 +5,49 @@ $(document).ready(function() {
     $('#titlebar').mouseleave(function() {
         $('#titlebar').fadeTo('slow', 0.5);
     });
+    $(".toggleMenu").css("display", "none");
+    $(".nav li").hover(function() {
+        $(this).addClass('hover');
+    }, function() {
+        $(this).removeClass('hover');
+    });
+    var ww = document.body.clientWidth;
+    if (ww < 800) {
+        $(".toggleMenu").css("display", "inline-block");
+        $(".nav li a").click(function() {
+            $(this).parent("li").toggleClass('hover');
+        });
+    } else {
+        $(".toggleMenu").css("display", "none");
+        $(".nav li").hover(function() {
+            $(this).addClass('hover');
+        }, function() {
+            $(this).removeClass('hover');
+        });
+    }
+    $(".nav li a").each(function() {
+        if ($(this).next().length > 0) {
+            $(this).addClass("parent");
+        };
+    })
+    if (ww < 800) {
+        $(".toggleMenu").css("display", "inline-block");
+        $(".nav li a.parent").click(function(e) {
+            e.preventDefault();
+            $(this).parent("li").toggleClass('hover');
+        });
+    } else {
+        ...
+    }
+    $(".toggleMenu").click(function(e) {
+        e.preventDefault();
+        $(".nav").toggle();
+    });
+     
+    if (ww < 800) {
+        $(".toggleMenu").css("display", "inline-block");
+        $(".nav").hide();
+    } else {
+        ...
+    }
 });
